a004b4214027b1084fe9f34d9c0d2a3f2cfa86bb
Erin Hinkle <erinhinx5@gmail.com>
2014-09-27 22:54:58 -0400

    welp?

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
-
+	$('.results').on('click', '.initiate', function() {
+		var sum = 0;
+		for (i=1; i<=100; i++) {
+			if ((i%3) === 0) {
+				if ((i%5) === 0) {
+					$('.results').append('<p>FIZZBUZZ!!</p>');	
+				} else $('.results').append('<p>FIZZ!</p>');
+			}
+			else if ((i%5) === 0) {
+				$('.results').append('<p>BUZZ!</p>');
+			} else {
+				sum = i;
+				$('.results').append('<p>' + sum + '</p>');
+			}
+		}
+	});
 });
